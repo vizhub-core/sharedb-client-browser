2239deb22182d16f883f68510eaacaed0d85c428
Curran <curran.kelleher@gmail.com>
2023-03-05 20:13:27 -0500

    Make build compatible with Vite

---
+++ src/ot-json1.js
@@ -0,0 +1,3 @@
+const json1 = require('ot-json1');
+const textUnicode = require('ot-text-unicode');
+module.exports = { json1, textUnicode };
---
+++ src/sharedb-client.js
@@ -0,0 +1,1 @@
+module.exports = require('sharedb/lib/client');
